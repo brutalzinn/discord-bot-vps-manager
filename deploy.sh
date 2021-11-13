9ead97a70f202a31149e0470cee91dd09a915548
Roberto <robertocpaes@gmail.com>
2021-11-12 22:59:31 -0300

    atualizando auto deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-pkill -f bot.py || echo 'ninguém rodando'
-python3 /home/brutalzinn/discord/bot.py
+pkill -f bot.py -c echo 'bobertos mortos.' &
+python3 /home/brutalzinn/discord/bot.py &
+echo 'Rodando o boberto...' &
+disown
