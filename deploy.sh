a9679736c8224c72b3f582c43209b0cb2ec018ea
Roberto <robertocpaes@gmail.com>
2021-11-12 23:01:27 -0300

    testando 2

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-pkill -f bot.py -c echo 'bobertos mortos.' &
+pkill -f bot.py -c &
+echo 'bobertos mortos.' &
 python3 /home/brutalzinn/discord/bot.py &
 echo 'Rodando o boberto...' &
 disown
