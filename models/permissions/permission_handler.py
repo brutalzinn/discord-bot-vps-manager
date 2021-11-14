8533cccbc9827b95c54b28f4483c90023a5078b9
Roberto <robertocpaes@gmail.com>
2021-11-13 22:39:31 -0300

    pequeno teste

--- models/permissions/permission_handler.py
+++ models/permissions/permission_handler.py
@@ -4,9 +4,12 @@ class permission_handler:
     def __init__(self, perm_register : permission_register):
         self.GetUsers = perm_register.get_user_permissions()
     def check_permission(self, author, nivel = 0):
+        exists = False
         for obj in self.GetUsers:
             print(f'{obj.discord_id}-{author}')
             if str(author) == obj.discord_id and nivel <= obj.nivel:
-                return True
+                exists = True
+                break
             else:
-                return False
+                exists = False
+        return exists
