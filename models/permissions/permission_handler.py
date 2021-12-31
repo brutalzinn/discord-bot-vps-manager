885cd420953e414ece206f82f721d25db9d9eedc
Roberto Paes <robertocpaes@gmail.com>
2021-12-31 18:29:34 -0300

    hotfix

--- models/permissions/permission_handler.py
+++ models/permissions/permission_handler.py
@@ -3,7 +3,7 @@ from models.permissions.permission_register import permission_register
 class permission_handler:
     def __init__(self, perm_register : permission_register):
         self.GetUsers = perm_register.get_user_permissions()
-    def (self, author, nivel = 0):
+    def check_permission(self, author, nivel = 0):
         exists = False
         for obj in self.GetUsers:
             if str(author) == obj.discord_id and nivel <= obj.nivel:
