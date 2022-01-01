4936691653e3f3b0364188d999b7c3a8fdcaff6a
Roberto Paes <robertocpaes@gmail.com>
2022-01-01 20:57:01 -0300

    tt

--- api.py
+++ api.py
@@ -71,20 +71,20 @@ def upload_file():
    if request.method == 'POST':
       f = request.files['file']
       directory = request.form.get('directory')
-      if not os.path.exists(os.path.join("web","data","cliente","files","files",directory)):
-         # os.mkdir(os.path.join("web","data","cliente","files","files",directory))
+      if not os.path.exists(os.path.join('web','data','cliente','files','files',directory)):
+         # os.mkdir(os.path.join('web','data','cliente','files','files',directory))
           os.umask(0)
-          os.makedirs(os.path.join("web","data","cliente","files","files",directory), mode=0o777)
+          os.makedirs(os.path.join('web','data','cliente','files','files',directory), mode=0o777)
       if f.filename.split('.')[1]  != 'zip':
         return Response(status=401)
-      file_zip = os.path.join("web","data","cliente","files","files", f.filename)
-      file_zip_out = os.path.join("web","data","cliente","files","files",directory)
+      file_zip = os.path.join('web','data','cliente','files','files', f.filename)
+      file_zip_out = os.path.join('web','data','cliente','files','files',directory)
       try:
          f.save(file_zip)
          Archive(file_zip).extractall(file_zip_out)
          #os.unlink(file_zip)
       except:
-         print("Deu erro.")
+         print('Deu erro.')
          return Response(status=401)
       return Response(status=200)
 		
