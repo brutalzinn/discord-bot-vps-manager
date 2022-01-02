3fcf9caeab3a5e45eaca95551e53fcde94a98922
Roberto Paes <robertocpaes@gmail.com>
2022-01-01 22:42:55 -0300

    testando versao ubuntu nova

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pkill -f boberto.py
-python -m pip install -r requirements.txt --user
+python3 -m pip install -r requirements.txt --user
 sleep 5
-python -u boberto.py >> error.log 2>&1 &
+python3 -u boberto.py >> error.log 2>&1 &
+
