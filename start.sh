b13ea2aebfb323c34f69706f485a472b908f8c5b
Roberto Paes <robertocpaes@gmail.com>
2022-01-01 23:57:14 -0300

    update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 pkill -f boberto.py
-python3 -m pip install -r requirements.txt --user
+python3.9 -m pip install -r requirements.txt --user
 sleep 5
-python3 -u boberto.py >> error.log 2>&1 &
-
+python3.9 -u boberto.py >> error.log 2>&1 &
