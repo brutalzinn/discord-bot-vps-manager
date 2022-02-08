e017797f6761d6819aee9f1d67bef35eeb646e4c
Roberto Paes <robertocpaes@gmail.com>
2022-02-08 15:12:31 -0300

    init cron job command sheduler

--- cronjob.py
+++ cronjob.py
@@ -1,3 +1,4 @@
+import json
 import pycron
 from datetime import datetime
 import time
@@ -18,7 +19,7 @@ def Start():
             job.desc = user._mapping['desc']
             job.server = user._mapping['server']
             job.expression = user._mapping['expression']
-            job.command = user._mapping['command']
+            job.command =  json.loads(user._mapping['command'])
             job.enabled = user._mapping['enabled']
             jobs.append(job)
             print(f'job created {job.enabled}-{job.name}')
@@ -28,6 +29,10 @@ def Start():
             if job.enabled == 1 and pycron.is_now(job.expression):
                 data_atual = datetime.now()
                 data_em_texto = data_atual.strftime('%d/%m/%Y %H:%M')
-                print(f'comando: {job.name} executando {job.command} em {job.server} {data_em_texto}')
+                if len(job.command) > 1:
+                    for c in job.command:
+                        print(f'comando: {job.name} executando {c} em {job.server} {data_em_texto}')
+                else:
+                    print(f'comando: {job.name} executando {job.command[0]} em {job.server} {data_em_texto}')
 
         time.sleep(60)
