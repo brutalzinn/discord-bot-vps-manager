0289bf2264681125681f23bd446f0945b95b4527
Roberto Paes <robertocpaes@gmail.com>
2022-02-08 21:22:38 -0300

    init discord command to handle jobs managment

--- comandos/shedule.py
+++ comandos/shedule.py
@@ -4,6 +4,7 @@ from models.commands.command_model import command_model
 from models.commands.command_register import command_register
 from models.commands.command_args import command_args
 from models.commands.command_args_register import command_args_register
+import discord
 
 class Dialogo:
     nome = 'NOME'
@@ -17,13 +18,51 @@ async def job(command : command_model, message, user, client):
 
     new_args = command.args[1:]
     passos = ['nome','desc','server','expresion','command','enabled']
-    await message_handler.send_message_normal('say hello')
-    def check(m):
-        return m.content == 'hello'
-    msg = await client.wait_for('message', check=check)
-    await message_handler.send_message_normal('Hello {.author}!'.format(msg))
+    def modo(m):
+        return isinstance(m.channel, discord.channel.DMChannel) and message.author != user and m.content == 'criar'
+    def private(m):
+        if isinstance(m.channel, discord.channel.DMChannel) and message.author != user:
+            return True
 
-  
+    await message_handler.send_message_private(message, user, 'digite criar/editar ou deletar para gerenciamento de jobs')
+    msg_modo = await client.wait_for('message', check=modo)
+    await message_handler.send_message_private(message, user,'Criando job no modo: {.content}'.format(msg_modo))
+
+    #get name of job
+
+    await message_handler.send_message_private(message, user,'Digite um nome para o job ser criado.')
+    msg_nome = await client.wait_for('message', check=private)
+    await message_handler.send_message_private(message, user,'Job: {.content}'.format(msg_nome))
+
+    #get  desc
+    await message_handler.send_message_private(message, user,'Digite uma descrição para o job ser criado.')
+    msg_desc = await client.wait_for('message', check=private)
+    await message_handler.send_message_private(message, user,'Descrição: {.content}'.format(msg_desc))
+
+    #get  server
+
+    await message_handler.send_message_private(message, user,'Digite um container para o job ser criado.')
+    msg_server = await client.wait_for('message', check=private)
+    await message_handler.send_message_private(message, user,'Container: {.content}'.format(msg_server))
+
+    #get  expression
+    await message_handler.send_message_private(message, user,'Digite um expresion para o job ser criado.')
+    msg_expression = await client.wait_for('message', check=private)
+    await message_handler.send_message_private(message, user,'Expressão cron: {.content}'.format(msg_expression))
+    
+    #get command
+
+    await message_handler.send_message_private(message, user,'Digite um command para o job ser criado.')
+    msg_command = await client.wait_for('message', check=private)
+    await message_handler.send_message_private(message, user,'Comando: {.content}'.format(msg_command))
+
+    #get enabled
+
+    await message_handler.send_message_private(message, user,'Digite um enabled para o job ser criado.')
+    msg_enabled = await client.wait_for('message', check=private)
+    await message_handler.send_message_private(message, user,'enabled: {.content}'.format(msg_enabled))
+
+
 def register(commands : command_register):
     args_register = command_args_register()
     command_model('job', method=job, descricao="criar/editar/deletar jobr", register=commands)
--- message_handler.py
+++ message_handler.py
@@ -2,9 +2,12 @@ from models.commands.command_model import command_model
 import discord
 
 async def send_message_private(message, user, resposta = ''):
+
     if isinstance(message.channel, discord.channel.DMChannel) and message.author != user:
         await message.channel.send(resposta)
+
 async def send_message_normal(message, user, resposta = ''):
         await message.channel.send(resposta)
+
 def isPrivate(message, user):
     return isinstance(message.channel, discord.channel.DMChannel) and message.author != user
