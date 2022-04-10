f0e44bf197577b0688614b78b0ec114710afbc8d
Roberto Paes <robertocpaes@gmail.com>
2022-04-10 11:22:44 -0300

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pkill -f boberto.py
-python3 -m pip install -r requirements.txt --force
+python3 -m pip install -r requirements.txt --upgrade --no-deps --force-reinstall
 sleep 5
 python3 -u boberto.py >> error.log 2>&1 &
