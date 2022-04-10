cd5535f2d7094567a9d63417272bf1aa7bac114e
Roberto Paes <robertocpaes@gmail.com>
2022-04-10 10:46:02 -0300

    fix dependencies to ubuntu server 20

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pkill -f boberto.py
-python3 -m pip install -r requirements.txt --user
+python3 -m pip install -r requirements.txt --force
 sleep 5
 python3 -u boberto.py >> error.log 2>&1 &
